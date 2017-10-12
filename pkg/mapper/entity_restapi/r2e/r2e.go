8fed9631602046acdaec3b815ff43a731e4b06da
zhuojie <zhuojie@checkr.com>
2017-10-12 14:13:05 -0700

    Add attachment for variant

--- pkg/mapper/entity_restapi/r2e/r2e.go
+++ pkg/mapper/entity_restapi/r2e/r2e.go
@@ -1,9 +1,12 @@
 package r2e
 
 import (
+	"fmt"
+
 	"github.com/checkr/flagr/pkg/entity"
 	"github.com/checkr/flagr/pkg/util"
 	"github.com/checkr/flagr/swagger_gen/models"
+	"github.com/davecgh/go-spew/spew"
 )
 
 // MapDistributions maps distribution
@@ -25,4 +28,24 @@ func MapDistribution(r *models.Distribution, segmentID uint) entity.Distribution
 		Bitmap:     r.Bitmap,
 	}
 	return e
+}
+
+// MapAttachment maps attachment
+func MapAttachment(a interface{}) (entity.Attachment, error) {
+	e := entity.Attachment{}
+
+	if a != nil {
+		m, ok := a.(map[string]interface{})
+		if !ok {
+			return e, fmt.Errorf("all key/value pairs should be string/string. invalid attachment format %s", spew.Sdump(a))
+		}
+		for k, v := range m {
+			s, ok := v.(string)
+			if !ok {
+				return e, fmt.Errorf("all key/value pairs should be string/string. invalid attachment format %s", spew.Sdump(a))
+			}
+			e[k] = s
+		}
+	}
+	return e, nil
 }
