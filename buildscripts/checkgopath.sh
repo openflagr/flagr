f2b4ba1b9c1810c50b9831e08d23b3b2a8159363
Uygar Yasar <uygar.yasar@dstvo.com>
2019-03-07 22:27:47 +0200

    * Fork changes.

--- buildscripts/checkgopath.sh
+++ buildscripts/checkgopath.sh
@@ -1,7 +1,7 @@
 main() {
     IFS=':' read -r -a paths <<< "$GOPATH"
     for path in "${paths[@]}"; do
-        flagr_path="$path/src/github.com/checkr/flagr"
+        flagr_path="$path/src/github.com/rexmont/flagr"
         if [ -d "$flagr_path" ]; then
             if [ "$flagr_path" -ef "$PWD" ]; then
                exit 0
