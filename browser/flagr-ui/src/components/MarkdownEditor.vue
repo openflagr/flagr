698d2c8fff23ffd6b3e56a7ee5344576ba78b2e1
zhuojie <zhuojie@checkr.com>
2020-03-16 18:57:12 -0700

    Add xss for mavonEditor

--- browser/flagr-ui/src/components/MarkdownEditor.vue
+++ browser/flagr-ui/src/components/MarkdownEditor.vue
@@ -14,6 +14,7 @@
 
 import { mavonEditor } from 'mavon-editor'
 import 'mavon-editor/dist/css/index.css'
+import * as xss from "xss"
 
 export default {
   name: 'editor',
@@ -47,7 +48,7 @@ export default {
       this.editorSettings.subfield = show
     },
     syncMarkdown (md) {
-      this.$emit('update:markdown', md)
+      this.$emit('update:markdown', xss(md))
     },
     save () {
       this.$emit('save')
