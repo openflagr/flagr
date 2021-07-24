d2a51a7bcc7670bb6c123982baa38064d0ddc686
Saxon Jensen <saxon.jensen@healthengine.com.au>
2021-07-25 00:21:29 +0800

    Switch to jest

--- browser/flagr-ui/test/ColourHashing.spec.js
+++ browser/flagr-ui/test/ColourHashing.spec.js
@@ -1,9 +1,8 @@
-import helpers from '@/helpers/helpers.js';
-import { expect } from 'chai';
+import helpers from '../src/helpers/helpers.js';
 
 describe('Colour hashing helper', () => {
-  it('should produce a valid css hsla string', () => {
+  test('should produce a valid css hsla string', () => {
     const randomString = Math.random().toString(36).substring(2, 15);
-    expect(helpers.stringToColour(randomString)).to.match(/hsla\((\d{1,3}), (\d{1,3}\%), (\d{1,3}\%), 1\)/);
+    expect(helpers.stringToColour(randomString)).toMatch(/hsla\((\d{1,3}), (\d{1,3}\%), (\d{1,3}\%), 1\)/);
   })
 });
