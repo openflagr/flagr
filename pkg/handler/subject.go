9ac5043d1bdd7ce542316c85982cd519e2a83b69
Zhuojie Zhou | rex <89176731+nothing0012@users.noreply.github.com>
2023-06-29 19:00:03 -0700

    Add cookie subject extractor (#536)
    * Add cookie subject extractor
    
    * Fix typo
    
    ---------
    
    Co-authored-by: Zhuojie Zhou <zhouzhuojie@gmail.com>

--- pkg/handler/subject.go
+++ pkg/handler/subject.go
@@ -26,6 +26,21 @@ func getSubjectFromRequest(r *http.Request) string {
 
 	} else if config.Config.HeaderAuthEnabled {
 		return r.Header.Get(config.Config.HeaderAuthUserField)
+	} else if config.Config.CookieAuthEnabled {
+		c, err := r.Cookie(config.Config.CookieAuthUserField)
+		if err != nil {
+			return ""
+		}
+		if config.Config.CookieAuthUserFieldJWTClaim != "" {
+			// for this case, we choose to skip the error check because just like HeaderAuthUserField
+			// in the future, we can extend this function to support cookie jwt token validation
+			// this assumes that the cookie we get already passed the auth middleware
+			token, _ := jwt.Parse(c.Value, func(token *jwt.Token) (interface{}, error) { return "", nil })
+			if claims, ok := token.Claims.(jwt.MapClaims); ok {
+				return util.SafeString(claims[config.Config.CookieAuthUserFieldJWTClaim])
+			}
+		}
+		return c.Value
 	}
 
 	return ""
--- pkg/handler/subject_test.go
+++ pkg/handler/subject_test.go
@@ -48,3 +48,31 @@ func TestGetSubjectFromOauthProxy(t *testing.T) {
 	r.Header.Set(config.Config.HeaderAuthUserField, "foo@example.com")
 	assert.Equal(t, getSubjectFromRequest(r.WithContext(ctx)), "foo@example.com")
 }
+
+func TestGetSubjectFromCookie(t *testing.T) {
+	var ctx = context.Background()
+	defer func() { config.Config.CookieAuthEnabled = false }()
+	config.Config.CookieAuthEnabled = true
+
+	t.Run("test HS256 happy codepath", func(t *testing.T) {
+		r, _ := http.NewRequest("GET", "", nil)
+		assert.Equal(t, getSubjectFromRequest(r), "")
+
+		r.AddCookie(&http.Cookie{
+			Name:  config.Config.CookieAuthUserField,
+			Value: "eyJhbGciOiJIUzI1NiIsImtpZCI6IjEyMzQ1In0.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW1haWwiOiJhYmNAZXhhbXBsZS5jb20iLCJpYXQiOjE1MTYyMzkwMjJ9.tzRXenFic8Eqg2awzO0eiX6Rozy_mmsJVzLJfUUfREI",
+		})
+		assert.Equal(t, getSubjectFromRequest(r.WithContext(ctx)), "abc@example.com")
+	})
+
+	t.Run("test HS256 empty claim", func(t *testing.T) {
+		r, _ := http.NewRequest("GET", "", nil)
+		assert.Equal(t, getSubjectFromRequest(r), "")
+
+		r.AddCookie(&http.Cookie{
+			Name:  config.Config.CookieAuthUserField,
+			Value: "eyJhbGciOiJIUzI1NiIsImtpZCI6IjEyMzQ1In0.eyJzdWIiOiIxMjM0NTY3ODkwIiwiaWF0IjoxNTE2MjM5MDIyfQ.C_YsEkcHa7aSVQILzJAayFgJk-sj1cmNWIWUm7m7vy4",
+		})
+		assert.Equal(t, getSubjectFromRequest(r.WithContext(ctx)), "")
+	})
+}
