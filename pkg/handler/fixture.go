861b42e61a4933f4c31c58694fca7bc6c46c9eda
Alexander Afanasyev <alexander.afanasyev@aviasales.ru>
2025-09-03 19:17:48 +0700

    Extend EvalCache export with a querying option

--- pkg/handler/fixture.go
+++ pkg/handler/fixture.go
@@ -24,3 +24,29 @@ func GenFixtureEvalCache() *EvalCache {
 
 	return ec
 }
+
+func GenFixtureEvalCacheWithFlags(flags []entity.Flag) *EvalCache {
+	idCache := make(map[string]*entity.Flag)
+	keyCache := make(map[string]*entity.Flag)
+	tagCache := make(map[string]map[uint]*entity.Flag)
+	for _, f := range flags {
+		idCache[util.SafeString(f.Model.ID)] = &f
+		keyCache[f.Key] = &f
+		for _, tag := range f.Tags {
+			if tagCache[tag.Value] == nil {
+				tagCache[tag.Value] = make(map[uint]*entity.Flag)
+			}
+			tagCache[tag.Value][f.ID] = &f
+		}
+	}
+
+	ec := &EvalCache{
+		cache: &cacheContainer{
+			idCache:  idCache,
+			keyCache: keyCache,
+			tagCache: tagCache,
+		},
+	}
+
+	return ec
+}
